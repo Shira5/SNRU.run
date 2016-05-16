0b7f99510d17877fc1f5193daa62e5a373dc0ee6
newly <newly05@hotmail.com>
2016-05-16 12:04:32 -0700

    SignUP finish

---
+++ app/src/main/java/appsnru/newly/snrurun/SignUP.java
@@ -0,0 +1,13 @@
+package appsnru.newly.snrurun;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUP extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
